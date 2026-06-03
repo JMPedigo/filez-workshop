8b6816ce5f3cecff9f816f856b6c2296097a2a5c
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-03 08:28:30 -0500

    Adds seed function

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,22 @@
 import db from "#db/client";
 
+import { createFile } from "./queries/files";
+import { createFolder } from "./queries/folders";
+
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
-async function seed() {
-  // TODO
+async function seedFolders() {
+  for (let i = 1; i <= 3; i++) {
+    const folder = await createFolder("Folder " + i);
+    for (let j = 1; j <= 5; j++) {
+      await createFile(
+        "File " + j,
+        Math.floor(Math.random() * 1000),
+        folder.id,
+      );
+    }
+  }
 }
