9c90ca5e0f3ad5310b542d063fb6e8bb35c8464b
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-03 08:36:13 -0500

    Seeds database with placeholder data, and passes all database tests

--- db/seed.js
+++ db/seed.js
@@ -1,14 +1,14 @@
 import db from "#db/client";
 
-import { createFile } from "./queries/files";
-import { createFolder } from "./queries/folders";
+import { createFile } from "#db/queries/files";
+import { createFolder } from "#db/queries/folders";
 
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
-async function seedFolders() {
+async function seed() {
   for (let i = 1; i <= 3; i++) {
     const folder = await createFolder("Folder " + i);
     for (let j = 1; j <= 5; j++) {
