5c2dcc6f39d7ab0d75a84742d6b44c6d6a9b5241
James Pedigo <ogidep.m.semaj@gmail.com>
2026-06-03 13:49:03 -0500

    Adds getFilesIncludingFolderName

--- db/queries/files.js
+++ db/queries/files.js
@@ -1,5 +1,6 @@
 import db from "#db/client";
 
+/** @returns the file created according to provided details */
 export async function createFile(name, size, folderID) {
   const sql = `
     INSERT INTO files
@@ -8,8 +9,24 @@ export async function createFile(name, size, folderID) {
         ($1, $2, $3)
     RETURNING *
     `;
+
   const {
     rows: [file],
   } = await db.query(sql, [name, size, folderID]);
   return file;
+}
+
+/** @returns an array of all files including the name of the containinf folder as folder_name */
+export async function getFilesIncludingFolderName() {
+  const sql = `
+    SELECT
+        files.*,
+        folders.name AS folder_name
+    FROM
+        files
+        JOIN folders ON folders.id = files.folder_id
+        `;
+
+  const { rows: files } = await db.query(sql);
+  return files;
 }
